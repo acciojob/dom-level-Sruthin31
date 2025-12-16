12e9aa88fa0314e0f76ab8cbf2b25a819996dcb7
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2025-12-16 19:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const n = document.getElementId('level');
+
+let total = 0;
+let current = n;
+
+while(current){
+	total++;
+	current = current.parentn;
+}
+alert("The level of the element is: "+total);
